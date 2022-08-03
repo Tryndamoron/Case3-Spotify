c427dd60245d4dcb506a48b7e8dd558c3fc7b84d
Tácio Franco <98967235+Tryndamoron@users.noreply.github.com>
2022-08-03 03:31:23 -0300

    Start: Spotify Clone

---
+++ spotify-2/pages/index.tsx
@@ -0,0 +1,23 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Sidebar from "../components/Sidebar";
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>Spotify 2.0</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="bg-black h-screen overflow-hidden">
+        <Sidebar/>
+        {/*main*/}
+      </main>
+     <div>{/*player*/}</div> 
+    </div>
+  );
+};
+
+export default Home;
